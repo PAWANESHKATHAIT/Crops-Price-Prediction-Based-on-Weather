7c5db42c6c4060b72c45cef96159872337826349
Yash Kuchhal <yashkuchhal27@gmail.com>
2024-11-20 23:58:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,39 +8,61 @@ model = joblib.load('average_price_model.pkl')
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
+ 
+@app.route('/predict', methods=['GET', 'POST'])   
 def predict():
-    # Getting user input from the form
-    state = request.form['State']
-    district = request.form['District']
-    market = request.form['Market']
-    commodity = request.form['Commodity']
-    grade = request.form['Grade']
-    temperature = int(request.form['Temperature'])
-    weather = request.form['Weather']
-    weather_condition = request.form['Weather_Condition']
-    
-    # Create a DataFrame for the input
-    input_data = {
-        'State': [state],
-        'District': [district],
-        'Market': [market],
-        'Commodity': [commodity],
-        'Grade': [grade],
-        'temperature': [temperature],
-        'weather': [weather],
-        'Weather_Condition': [weather_condition]
-    }
-    
-    input_df = pd.DataFrame(input_data)
-    
-    # Predict the average price
-    predicted_price = model.predict(input_df)
-    
-    return render_template('index.html', predicted_price=predicted_price[0])
+    if request.method == 'POST':
+        # Getting user input from the form
+        state = request.form['State']
+        district = request.form['District']
+        market = request.form['Market']
+        commodity = request.form['Commodity']
+        grade = request.form['Grade']
+        temperature = int(request.form['Temperature'])
+        weather = request.form['Weather']
+        weather_condition = request.form['Weather_Condition']
+        
+        # Create a DataFrame for the input
+        input_data = {
+            'State': [state],
+            'District': [district],
+            'Market': [market],
+            'Commodity': [commodity],
+            'Grade': [grade],
+            'temperature': [temperature],
+            'weather': [weather],
+            'Weather_Condition': [weather_condition]
+        }
+        
+        input_df = pd.DataFrame(input_data)
+        
+        # Predict the average price
+        predicted_price = model.predict(input_df)
+        
+        return render_template('predict.html', predicted_price=predicted_price[0])
+    return render_template('predict.html')
+
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/product-single')
+def product_single():
+    return render_template('product-single.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
